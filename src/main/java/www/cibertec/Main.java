01f79dd2a72bdb63ba2641205eb19228e8165f48
Gian Carlo Quiroz Guzman <gquirozg@nttdata.com>
2026-08-20 20:32:07 -0500

    Ejercicio realizado del proyecto final

--- src/main/java/www/cibertec/Main.java
+++ src/main/java/www/cibertec/Main.java
@@ -1,41 +1,17 @@
 package www.cibertec;
 
 public class Main {
+
     static void main() {
 
-        String producto = "Polo";
-        int cantidad = 3;
-        double precio = 50;
+        // Programa para calcular promedio y suma de notas
+        saludar();
 
-        double total = calcularTotal(precio, cantidad);
-
-        if (total >= 100) {
-            System.out.println("Compra grande");
-        } else {
-            System.out.println("Compra pequeña");
-        }
-
-        for (int i = 1; i <= cantidad; i++) {
-            System.out.println("Producto vendido: " + i);
-        }
-
-        int opcion = 1;
-
-        switch (opcion) {
-            case 1:
-                System.out.println(producto + " - Total: " + total);
-                break;
-            case 2:
-                System.out.println("Precio: " + precio);
-                break;
-            default:
-                System.out.println("Opción no válida");
-        }
 
     }
 
-    static double calcularTotal(double precio, int cantidad) {
-        return precio * cantidad;
+    static void saludar(){
+        System.out.println("Hola, bienvenido!");
     }
 
 }
